c85520706f9ddd9e4553169ff9202bbe31a4d1a2
hotcat190 <nguyendinhnguyen151204@gmail.com>
2025-07-12 20:57:14 +0700

    chore: refactor aptos adapter

--- src/modules/tracer-engine/data-adapter/aptos-adapter.ts
+++ src/modules/tracer-engine/data-adapter/aptos-adapter.ts
@@ -1,4 +1,5 @@
 import { Edge } from "@guaritos/tracer-engine/dist/engine/items/ttr_defs";
+import { AccountTransaction } from "src/modules/aptos";
 
 enum EventTypes {
     GAS_FEE_EVENT  = `0x1::aptos_coin::GasFeeEvent`,
@@ -13,27 +14,33 @@ class SmartContractTypes {
 }
 
 export class AptosAdapter {
-    parse_query_result(
-        aptos_query_result: any,
-        include_swap: boolean,
+    static parse_query_result(
+        account_transactions: AccountTransaction[],
         swap_options?: {
-            contract_addresses: Set<string>,
+            include_swap: boolean,
+            filters?: {
+                contract_addresses: Set<string>,
+            },
         },
-    ): Edge[] {
-        if (include_swap && typeof(swap_options) === undefined) throw new Error("Enabling include_swap without specifying swap_options is not allowed.")
+    ): Edge[] {        
         let edges: Edge[] = [];
-        const account_transactions = aptos_query_result.data.account_transactions;
         
         for (let t = 0; t < account_transactions.length; t++) {
             const transaction = account_transactions[t];
 
             const sender = transaction.user_transaction.sender;
-            const timestamp = transaction.user_transaction.timestamp;
-            const version = transaction.user_transaction.version;
+            
+            // "YYYY-MM-DD{T}HH:mm:ss:sssZ" -> epoch
+            const timestamp = Date.parse(transaction.user_transaction.timestamp + "Z");
+
+            // Store hash as version
+            const version = transaction.user_transaction.version.toString();
 
             const fungible_asset_activities = transaction.fungible_asset_activities;
 
             let isSwap = false;
+
+            const contract_address = transaction.user_transaction.entry_function_contract_address;
 
             for (let i = 0; i < fungible_asset_activities.length; i++) {
                 const event = fungible_asset_activities[i];
@@ -46,9 +53,7 @@ export class AptosAdapter {
                     isSwap = true;
                 }
 
-                const contract_address = event.entry_function_id_str.split('::')[0];
-
-                if (include_swap && isSwap && event.owner_address === sender && swap_options.contract_addresses.has(contract_address)) {
+                if (swap_options.include_swap && isSwap && event.owner_address === sender && swap_options.filters.contract_addresses.has(contract_address)) {
                     if (event.type === EventTypes.WITHDRAW_EVENT) {
                         edges.push({
                             from: sender,
